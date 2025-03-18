b859d24788e88b2612861b83d17f19634122f5b9
Alex Nelson <alexander.nelson@nist.gov>
2025-03-18 15:27:44 -0400

    Bump prerelease pointers; relax develop validations until CASE 1.4.0
    A follow-on patch will regenerate Make-managed files.
    
    References:
    * https://github.com/ucoProject/UCO/issues/629
    
    Signed-off-by: Alex Nelson <alexander.nelson@nist.gov>

--- catalog/postvisit.mk
+++ catalog/postvisit.mk
@@ -127,13 +127,14 @@ kb-all_validation.ttl: \
 	rm __$@
 	mv _$@ $@
 
+# TODO - After release of CASE 1.4.0, use --allow-infos instead of --allow-warnings.
 kb-all_validation-develop.ttl: \
   $(top_srcdir)/dependencies/CASE-develop.ttl \
   kb-all.ttl
 	rm -f __$@ _$@
 	source $(top_srcdir)/venv/bin/activate \
 	  && case_validate \
-	    --allow-infos \
+	    --allow-warnings \
 	    --built-version none \
 	    --format turtle \
 	    --inference rdfs \
